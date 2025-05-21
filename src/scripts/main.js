27308ceb6cca3930a4c14788202fca945227a448
Solodovsky <toxa2h@bk.ru>
2025-05-21 22:05:27 +0300

    add skript to main.js file and impove styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuLinks = document.querySelectorAll(".hero-section__header__icons a");
+  const menuButton = menuLinks[2];
+  const burgerMenu = document.getElementById("burgerMenu");
+  const closeBtn = document.getElementById("closeMenuBtn");
 
+  menuButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    burgerMenu.classList.add("active");
+    document.body.classList.add("menu-open");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    burgerMenu.classList.remove("active");
+    document.body.classList.remove("menu-open");
+  });
+});
